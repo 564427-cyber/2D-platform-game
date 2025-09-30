d8e67044a9f95f483cf9a7383bd630764c1b1bd4
564427-cyber <564427@escg.ac.uk>
2025-09-30 11:06:31 +0100

    added coin

--- Assets/script/EnemyScript.cs
+++ Assets/script/EnemyScript.cs
@@ -88,14 +88,7 @@ public class EnemyMove : MonoBehaviour
     }
 
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            Destroy(gameObject);
 
-        }
-    }
 
 
 
